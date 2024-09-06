7b7f222ab687748316f216f4b017a42ab3262084
Narayan Bhattarai <nayaranbhattarai2003@gmail.com>
2024-09-06 20:43:03 +0300

    Add files via upload

---
+++ APP....java
@@ -0,0 +1,49 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int age = 18;
+                if (age >=18){
+                    System.out.println("you can drive a car");
+                }
+                else{
+                    System.out.println("you are not eligible to drive");
+                }
+
+
+   int age1 =10;
+   int age2 =20;
+   int age3 =30;
+   int age4 =40;
+   int age5 =50;
+   int age6 =60;
+   int age7 =70;
+   int age8 =80;
+   int age9 =90;
+   int age10 =100;
+   int age11=110;
+   int age12=120;
+if ( age1== 10 || age2==20|| age3==30|| age4==40|| age5==50|| age6==60|| age7==70|| age8==80|| age9==90|| age10==100|| age11==110|| age12==120)
+{
+    System.out.println("you can celebrate anniversary party");
+    }
+    else{
+        System.out.println("you cannot celebrate anniversary party");
+ }
+
+        int age13=100;
+        if(age13==100){
+            System.out.println("provide three line congratulations");
+        }else
+             {
+                System.out.println("donot provide congratulations");
+                        }
+        
+        int age14=40;
+        int age24=50;
+        if(age14>=40 && age24<=50){
+        System.out.println("wish a happy mid life");}else{
+
+            System.out.println("don't wish happy mid life");
+        }
+        
+}}
